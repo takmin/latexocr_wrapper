aa3a40c74fdc2e2a38908a3fbdc0580cb4c93786
Minagawa Takuya <z.takmin@gmail.com>
2024-10-11 14:55:30 +0900

    easyocrという記載ミスをlatexocrへ変更

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -13,7 +13,7 @@ OUTPUT_ABS_PATH=$(readlink -f "$OUTPUT_PATH")
 docker run --gpus all -d --rm --name latexocr_runner --shm-size=256m -v ${INPUT_ABS_PATH}:/root/input_dir/ -v ${JSON_ABS_PATH}:/root/json_dir/ -v ${OUTPUT_ABS_PATH}:/root/output_dir/ -i latexocr-wrapper:latest
 
 # コンテナ内でコマンドを実行
-docker exec -it latexocr_runner bash -c "python /root/easyocr/proc_latexocr.py /root/input_dir /root/json_dir /root/output_dir"
+docker exec -it latexocr_runner bash -c "python /root/latexocr/proc_latexocr.py /root/input_dir /root/json_dir /root/output_dir"
 
 # コンテナを停止して削除
 docker stop latexocr_runner
